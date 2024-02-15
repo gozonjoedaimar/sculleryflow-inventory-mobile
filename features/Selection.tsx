fd9a4521c09154a2d221bf54946f7aec9ae93f46
gozonjoedaimar <gozonjoedaimar@gmail.com>
2024-02-15 18:10:09 +0800

    Add low inventory section to Selection component

--- features/Selection.tsx
+++ features/Selection.tsx
@@ -14,6 +14,18 @@ export default function Selection({ navigation }: HomeProps) {
                     <Item name="kitchen" navigation={navigation} />
                     <Item name="stockroom" navigation={navigation} />
                 </StyledView>
+                <StyledView className="w-80 m-auto mt-7">
+                    <StyledView className="-mx-1">
+                        <StyledText className="text-xl text-black">Low Inventory...</StyledText>
+                        <StyledView className="mt-1">
+                            <LowInventoryItem />
+                            <LowInventoryItem />
+                            <LowInventoryItem />
+                            <LowInventoryItem />
+                            <LowInventoryItem />
+                        </StyledView>
+                    </StyledView>
+                </StyledView>
             </ScrollView>
         </SafeAreaView>
     )
@@ -30,9 +42,20 @@ function Item({ name, navigation }: ItemProps) {
             onPress={ () => !!navigation && navigation.navigate('Inventory', { type: name }) }
             className="w-40 h-28 ml-3 flex flex-row justify-end items-center bg-gray-300 rounded-lg active:opacity-90"
         >
-            <StyledView className="px-3 py-1 bg-green-600 rounded-tl-lg rounded-bl-lg">
+            <StyledView className="px-2 py-0 bg-green-600 rounded-tl-lg rounded-bl-lg">
                     <StyledText className="text-lg text-white capitalize">{ name }</StyledText>
             </StyledView>
         </StyledPressable>
+    );
+}
+
+function LowInventoryItem() {
+    return (
+        <StyledView className="h-11 rounded-lg bg-red-500 mt-3">
+            <StyledView className="flex flex-row w-full px-3 h-full justify-between items-center">
+                <StyledText className="text-white">Lettuce</StyledText>
+                <StyledText className="text-white">1 pack remaining</StyledText>
+            </StyledView>
+        </StyledView>
     );
 }
